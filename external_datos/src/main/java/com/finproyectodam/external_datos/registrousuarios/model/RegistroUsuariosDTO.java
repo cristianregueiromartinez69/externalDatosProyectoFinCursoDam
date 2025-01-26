58203ef15c023031fb528bba8e5c984bec92349d
cristian <cregueiromartinez@danielcastelao.org>
2025-01-26 11:24:01 +0100

    clase registro de usuarios

--- external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/model/RegistroUsuariosDTO.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/model/RegistroUsuariosDTO.java
@@ -2,5 +2,42 @@ package com.finproyectodam.external_datos.registrousuarios.model;
 
 public class RegistroUsuariosDTO {
 
+    private String userName;
+    private String correo;
+    private String password;
+
+    public RegistroUsuariosDTO(String userName, String correo, String password) {
+        this.userName = userName;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public RegistroUsuariosDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 }
