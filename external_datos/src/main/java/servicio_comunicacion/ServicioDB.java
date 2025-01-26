fc561a68a73756504f9632671bd9572447396258
cristian <cregueiromartinez@danielcastelao.org>
2025-01-26 11:17:24 +0100

    cambiando a otra rama para hacer registro y servicio de comunicación

---
+++ external_datos/src/main/java/servicio_comunicacion/ServicioDB.java
@@ -0,0 +1,17 @@
+package servicio_comunicacion;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "relationaldb", url = "http://localhost:8081")
+public interface ServicioDB {
+
+    /*
+    @PostMapping("/relationaldb/spotify/registrousuarios")
+    void registerUsers(@RequestBody )
+    */
+
+}
