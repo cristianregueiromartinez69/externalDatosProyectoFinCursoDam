f53dae52f6a3830c793a4fdb7ef43fbe90d12591
cristian <cregueiromartinez@danielcastelao.org>
2025-01-26 10:58:59 +0100

    commit inicial aplicación

---
+++ external_datos/src/main/java/com/finproyectodam/external_datos/ExternalDatosApplication.java
@@ -0,0 +1,17 @@
+package com.finproyectodam.external_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalDatosApplication.class, args);
+	}
+
+	/*
+	  TODO: inicio y registro de usuarios en la aplicación
+	 */
+
+}
