4ac8bea98bfead49b1043a61dfc05567c25e1f7e
cristian <cregueiromartinez@danielcastelao.org>
2025-01-26 11:37:45 +0100

    rest controller registro usuarios

--- external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/controller/RegistroUsuariosRestController.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/controller/RegistroUsuariosRestController.java
@@ -1,4 +1,33 @@
 package com.finproyectodam.external_datos.registrousuarios.controller;
 
+import com.finproyectodam.external_datos.registrousuarios.model.RegistroUsuariosDTO;
+import com.finproyectodam.external_datos.registrousuarios.service.ServicioRegistroUsuarios;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spotify/")
 public class RegistroUsuariosRestController {
+
+    private final ServicioRegistroUsuarios servicioRegistroUsuarios;
+
+    public RegistroUsuariosRestController(ServicioRegistroUsuarios servicioRegistroUsuarios) {
+        this.servicioRegistroUsuarios = servicioRegistroUsuarios;
+    }
+
+    @PostMapping("registrousuarios")
+    public ResponseEntity<String> registroUsuariosSpotify(@RequestBody RegistroUsuariosDTO registroUsuariosDTO) {
+
+        try{
+            servicioRegistroUsuarios.registerUsuariosService(registroUsuariosDTO);
+            return ResponseEntity.ok("Usuario registrado correctamente");
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
 }
