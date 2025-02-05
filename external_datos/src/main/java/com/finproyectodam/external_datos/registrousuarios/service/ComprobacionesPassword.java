9ba5dc5ef5594d9c209cea751b08ac41712c6319
Cristian <cregueiromartinez@danielcastelao.org>
2025-02-05 10:58:07 +0100

    haciendo comprobaciones password

---
+++ external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/service/ComprobacionesPassword.java
@@ -0,0 +1,55 @@
+package com.finproyectodam.external_datos.registrousuarios.service;
+
+public class ComprobacionesPassword {
+
+    /**
+     * Metodo para comprobar si la contraseña del usuario tiene 12 o más caracteres
+     * @param password la contraseña proporcionada
+     * @return true o false si cumple los requisitios
+     */
+    public boolean checkLengthPassword(String password){
+
+        return password.length() >= 12;
+    }
+
+    /**
+     * Metodo para comprobar si la contraseña introducida tiene al menos 1 caracter especial
+     * @param password la contraseña introducida
+     * @return true o false dependiendo de la contraseña
+     */
+    public boolean checkPasswordHasStrangeThings(String password){
+        //array con los caracteres especiales
+        char[] caracteres = {'!', '|', '@', '"', '#', '·', '~', '$', '%', '€','¬','&','/','(',')','=','?','¿'
+                ,'¡','<','>','+','-','*','^','[',']','¨','{','}',',',';',':','_','`','´'};
+
+        return extracted(password, caracteres);
+    }
+
+    /**
+     * Metodo para comprobar que la contraseña tiene al menos 1 número
+     * @param password la contraseña introducida
+     * @return true o false dependiendo de la contraseña proporcionada
+     */
+    public boolean checkPaswordHasNumbers(String password){
+
+        //array de numeros
+        char [] numbers = {'1','2','3','4','5','6','7','8','9','0'};
+
+        return extracted(password, numbers);
+    }
+
+    private static boolean extracted(String password, char[] numbers) {
+        //array de letras de la contraseña
+        char [] passwordChars = password.toCharArray();
+
+        //bucle anidado para comprobar la coincidencia
+        for (char number : numbers) {
+            for (char passwordChar : passwordChars) {
+                if (number == passwordChar) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
