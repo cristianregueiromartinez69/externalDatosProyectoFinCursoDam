1de3e9204dd588e89a489542ccff46fd7b751c81
Cristian <cregueiromartinez@danielcastelao.org>
2025-02-05 10:45:04 +0100

    test y excepciones hechas

---
+++ external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/excepciones/EmailException.java
@@ -0,0 +1,7 @@
+package com.finproyectodam.external_datos.registrousuarios.excepciones;
+
+public class EmailException extends RuntimeException {
+    public EmailException(String message) {
+        super(message);
+    }
+}
--- external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/service/ServicioRegistroUsuarios.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/registrousuarios/service/ServicioRegistroUsuarios.java
@@ -1,5 +1,6 @@
 package com.finproyectodam.external_datos.registrousuarios.service;
 
+import com.finproyectodam.external_datos.registrousuarios.excepciones.EmailException;
 import com.finproyectodam.external_datos.registrousuarios.excepciones.UserNameException;
 import com.finproyectodam.external_datos.registrousuarios.model.RegistroUsuariosDTO;
 import com.finproyectodam.external_datos.servicio_comunicacion.ServicioDB;
@@ -18,9 +19,13 @@ public class ServicioRegistroUsuarios {
 
     public void registerUsuariosService(RegistroUsuariosDTO registroUsuariosDTO) throws UserNameException{
         ComprobacionesUserName comrpUser = new ComprobacionesUserName();
+        ComprobacionesEmail comrpEmail = new ComprobacionesEmail();
 
         if(!comrpUser.authenticationRegisterName(registroUsuariosDTO.getUserName())){
             throw new UserNameException("Username incorrecto, introduce otro");
+        }
+        else if(comrpEmail.authenticationRegisterEmail(registroUsuariosDTO.getCorreo())){
+            throw new EmailException("Correo incorrecto, introduce otro");
         }
         else{
             servicioDB.registerUsers(registroUsuariosDTO);
