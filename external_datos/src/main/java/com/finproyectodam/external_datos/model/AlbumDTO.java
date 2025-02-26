14a0792f96ff196b7a988e742a814a7427e0e47b
cristian <cregueiromartinez@danielcastelao.org>
2025-02-26 14:22:34 +0100

    dtos hechas

--- external_datos/src/main/java/com/finproyectodam/external_datos/model/AlbumDTO.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/model/AlbumDTO.java
@@ -1,4 +1,73 @@
 package com.finproyectodam.external_datos.model;
 
+
+
+import com.finproyectodam.external_datos.artistas.model.ArtistaDTO;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class AlbumDTO {
+
+
+
+
+    private String titulo;
+
+
+    private Integer anolanz;
+
+
+    private ArtistaDTO artistaid;
+
+    private Set<CancionDTO> canciones = new LinkedHashSet<>();
+
+    public AlbumDTO(String titulo, Integer anolanz, ArtistaDTO artistaid, Set<CancionDTO> canciones) {
+        this.titulo = titulo;
+        this.anolanz = anolanz;
+        this.artistaid = artistaid;
+        this.canciones = canciones;
+    }
+
+    public AlbumDTO(String titulo, Integer anolanz) {
+        this.titulo = titulo;
+        this.anolanz = anolanz;
+    }
+
+    public AlbumDTO() {
+    }
+
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAnolanz() {
+        return anolanz;
+    }
+
+    public void setAnolanz(Integer anolanz) {
+        this.anolanz = anolanz;
+    }
+
+    public ArtistaDTO getArtistaid() {
+        return artistaid;
+    }
+
+    public void setArtistaid(ArtistaDTO artistaid) {
+        this.artistaid = artistaid;
+    }
+
+    public Set<CancionDTO> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(Set<CancionDTO> canciones) {
+        this.canciones = canciones;
+    }
 }
--- external_datos/src/main/java/com/finproyectodam/external_datos/model/CancionDTO.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/model/CancionDTO.java
@@ -1,4 +1,96 @@
 package com.finproyectodam.external_datos.model;
 
+
+
+import com.finproyectodam.external_datos.artistas.model.ArtistaDTO;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class CancionDTO {
+
+
+
+
+    private String titulo;
+
+
+    private Integer duracion;
+
+
+    private String urlcancion;
+
+    private ArtistaDTO artistaid;
+
+
+    private AlbumDTO albumid;
+
+
+    private Set<PlayListDTO> playlists = new LinkedHashSet<>();
+
+    public CancionDTO(String titulo, Integer duracion, String urlcancion, ArtistaDTO artistaid, AlbumDTO albumid, Set<PlayListDTO> playlists) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.urlcancion = urlcancion;
+        this.artistaid = artistaid;
+        this.albumid = albumid;
+        this.playlists = playlists;
+    }
+
+    public CancionDTO(String titulo, Integer duracion, String urlcancion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.urlcancion = urlcancion;
+    }
+
+    public CancionDTO() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getUrlcancion() {
+        return urlcancion;
+    }
+
+    public void setUrlcancion(String urlcancion) {
+        this.urlcancion = urlcancion;
+    }
+
+    public ArtistaDTO getArtistaid() {
+        return artistaid;
+    }
+
+    public void setArtistaid(ArtistaDTO artistaid) {
+        this.artistaid = artistaid;
+    }
+
+    public AlbumDTO getAlbumid() {
+        return albumid;
+    }
+
+    public void setAlbumid(AlbumDTO albumid) {
+        this.albumid = albumid;
+    }
+
+    public Set<PlayListDTO> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(Set<PlayListDTO> playlists) {
+        this.playlists = playlists;
+    }
 }
