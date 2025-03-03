8ac719a3e55fdc660330b7151c8d8deb3efade3d
cristian <cregueiromartinez@danielcastelao.org>
2025-03-03 12:06:00 +0100

    generando dto token

---
+++ external_datos/src/main/java/com/finproyectodam/external_datos/model/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.finproyectodam.external_datos.model;
+
+public class TokenDTO {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
