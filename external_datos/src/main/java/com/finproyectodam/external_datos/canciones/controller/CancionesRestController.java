b765ffe538b91d96e9586bccbcbad7d80b1f4b4b
cristian <cregueiromartinez@danielcastelao.org>
2025-03-11 17:31:08 +0100

    rest controller de canciones

--- external_datos/src/main/java/com/finproyectodam/external_datos/canciones/controller/CancionesRestController.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/canciones/controller/CancionesRestController.java
@@ -1,4 +1,57 @@
 package com.finproyectodam.external_datos.canciones.controller;
 
+import com.finproyectodam.external_datos.canciones.service.CancionesService;
+import com.finproyectodam.external_datos.model.CancionDTO;
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * RestController de las canciones
+ * @author cristian && Joel
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/spotify/canciones")
 public class CancionesRestController {
+
+    //servicio de las canciones
+    private final CancionesService cancionesService;
+
+    /**
+     * Constructor de la clase
+     * @param cancionesService el servicio de las canciones
+     */
+    public CancionesRestController(CancionesService cancionesService) {
+        this.cancionesService = cancionesService;
+    }
+
+    /**
+     * Metodo get para obtener todas las canciones de la base de datos
+     * @return la lista de canciones o null
+     */
+    @GetMapping("/info")
+    public ResponseEntity<List<CancionDTO>> getAllSongsController(){
+        try{
+            List<CancionDTO> cancionDTOList = cancionesService.getAllSongs();
+            if(cancionDTOList.isEmpty()){
+                return ResponseEntity.noContent().build();
+            }
+            else{
+                return ResponseEntity.ok(cancionDTOList);
+            }
+        }catch (FeignException fe) {
+            if (fe.status() == HttpStatus.BAD_REQUEST.value()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.noContent().build();
+    }
 }
