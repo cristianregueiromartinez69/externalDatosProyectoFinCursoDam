527c2f4542601c07a30019759acd92fd0f4eadb0
cristian <cregueiromartinez@danielcastelao.org>
2025-03-12 16:23:32 +0100

    servicio de random playlist

--- external_datos/src/main/java/com/finproyectodam/external_datos/randomplaylist/service/RandomPLaylistService.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/randomplaylist/service/RandomPLaylistService.java
@@ -1,4 +1,33 @@
 package com.finproyectodam.external_datos.randomplaylist.service;
 
+import com.finproyectodam.external_datos.model.PlayListDTO;
+import com.finproyectodam.external_datos.servicio_comunicacion.ServicioDB;
+import org.springframework.stereotype.Service;
+
+/**
+ * Servicio de random playlist
+ * @author cristian && Joel
+ * @version 1.0
+ */
+@Service
 public class RandomPLaylistService {
+
+    //atributo del servicio de comunicacion
+    private final ServicioDB servicioDB;
+
+    /**
+     * Constructor de la clase
+     * @param servicioDB el servicio de comunicacion
+     */
+    public RandomPLaylistService(ServicioDB servicioDB) {
+        this.servicioDB = servicioDB;
+    }
+
+    /**
+     * Metodo para obtener una playlist aleatoria al entrar a la web
+     * @return la playlist aleatoria
+     */
+    public PlayListDTO getRandomPlayList() {
+        return servicioDB.getInitPlaylistSpotify();
+    }
 }
