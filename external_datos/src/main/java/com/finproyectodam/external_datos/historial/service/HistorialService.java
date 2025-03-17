3f5469059a19cd2d5b1ace430db1d47ccce23573
Cristian <cregueiromartinez@danielcastelao.org>
2025-03-17 11:04:38 +0100

    servicio del historial

--- external_datos/src/main/java/com/finproyectodam/external_datos/historial/service/HistorialService.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/historial/service/HistorialService.java
@@ -1,4 +1,32 @@
 package com.finproyectodam.external_datos.historial.service;
 
+import com.finproyectodam.external_datos.servicio_comunicacion.ServicioDB;
+import org.springframework.stereotype.Service;
+
+/**
+ * Servicio del historial de guardado de canciones
+ * @author cristian
+ * @version
+ */
+@Service
 public class HistorialService {
+
+    //atributo del servicio de comunicacion
+    private final ServicioDB servicioDB;
+
+    /**
+     * Constructor de la clase
+     * @param servicioDB el servicio de comunicacion
+     */
+    public HistorialService(ServicioDB servicioDB) {
+        this.servicioDB = servicioDB;
+    }
+
+    /**
+     * Metodo para guardar la cancion cuando el usuario le de al play
+     * @param id el id de la cancion
+     */
+    public void saveCancionHistorialService(Integer id){
+        servicioDB.saveCancionHistorial(id);
+    }
 }
