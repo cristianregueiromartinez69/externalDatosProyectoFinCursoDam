c1df16b83640c56ad2366fcdc751746fe90df7b3
Cristian <cregueiromartinez@danielcastelao.org>
2025-03-31 08:47:35 +0200

    servicio de obtencion del historial

--- external_datos/src/main/java/com/finproyectodam/external_datos/historial/service/HistorialGuardadoService.java
+++ external_datos/src/main/java/com/finproyectodam/external_datos/historial/service/HistorialGuardadoService.java
@@ -1,7 +1,13 @@
 package com.finproyectodam.external_datos.historial.service;
 
+import com.finproyectodam.external_datos.model.CancionDTO;
+import com.finproyectodam.external_datos.model.HistorialDTO;
+import com.finproyectodam.external_datos.servicio_comunicacion.ServicioDB;
 import com.finproyectodam.external_datos.servicio_comunicacion.ServicioMongo;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Servicio del historial de guardado de canciones
@@ -13,13 +19,15 @@ public class HistorialGuardadoService {
 
     //atributo del servicio de comunicacion
     private final ServicioMongo servicioMongo;
+    private final ServicioDB servicioDB;
 
     /**
      * Constructor de la clase
      * @param servicioMongo el servicio de comunicacion
      */
-    public HistorialGuardadoService(ServicioMongo servicioMongo) {
+    public HistorialGuardadoService(ServicioMongo servicioMongo, ServicioDB servicioDB) {
         this.servicioMongo = servicioMongo;
+        this.servicioDB = servicioDB;
     }
 
     /**
@@ -29,4 +37,27 @@ public class HistorialGuardadoService {
     public void saveCancionHistorialService(Integer idCancion){
         servicioMongo.saveCancionHistorial(idCancion);
     }
+
+    /**
+     * Metodo para obtener el historial de mis canciones
+     * @return la lista de historial de canciones
+     */
+    private List<HistorialDTO> getAllHistorialService(){
+        return servicioMongo.getAllHistorial();
+    }
+
+    /**
+     * Metodo para obtener la información de todas las canciones del historial
+     * @param historial una lista del historial
+     * @return la lista de canciones
+     */
+    public List<CancionDTO> getCancionesHistorial(List<HistorialDTO> historial){
+        List<CancionDTO> cancionesHistorial = new ArrayList<>();
+        for (HistorialDTO historialDTO : historial) {
+            CancionDTO cancionDTO = servicioDB.getCancionById(historialDTO.getIdCancion());
+            cancionesHistorial.add(cancionDTO);
+        }
+        return cancionesHistorial;
+    }
+
 }
